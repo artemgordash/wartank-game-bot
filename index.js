f65e1aa708abb7b7f5828bbd55287dea1734ee53
Artem Gordash <MY_NAME@example.com>
2023-02-23 23:31:22 +0200

    try to fix

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from 'express';
+import path from 'path';
 import initPuppeteer from './puppeteer/index';
 
 const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
-  res.send('Hello world');
+  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
   // initPuppeteer();
   console.log(`Server running at http://localhost:${port}\n\n`);
 });
 
-export default app;
+module.exports = app;
